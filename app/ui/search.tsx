6697c859eaaab006db3c7daf541d2f56f92f8e9d
Michael Toulouse <wrycoder@ameritech.net>
2024-11-07 06:30:39 -0500

    Put placeholder in a generic place

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -30,6 +30,7 @@ export default function Search() {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
+        placeholder="Search this site"
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
